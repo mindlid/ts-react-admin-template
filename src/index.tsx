71900ac06438fea958e34af9b59d2ed7480f23a7
saulpalv <19735362+saulpalv@users.noreply.github.com>
2021-10-02 12:29:01 -0600

    first

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { App } from './App';
+import { Admin, Resource, ListGuesser } from 'react-admin'
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
