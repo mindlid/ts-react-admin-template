2ff56399080716f1714cf4986f6b771fba758f15
saulpalv <19735362+saulpalv@users.noreply.github.com>
2021-10-02 13:37:54 -0600

    added users resource

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
-import { Admin } from 'react-admin'
+import { Admin, Resource, ListGuesser, List, Datagrid, TextField, EmailField, ResourceComponentProps } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server'
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
+export const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
+
+export const App: React.FC = () =>
+    <Admin dataProvider={dataProvider} >
+        <Resource name="users" list={UserList} />
+    </Admin>
 
 
-export const App: React.FC = () => <Admin dataProvider={dataProvider} />
+export const UserList: React.FC<ResourceComponentProps> = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="address.street" />
+            <TextField source="phone" />
+            <TextField source="website" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+)
